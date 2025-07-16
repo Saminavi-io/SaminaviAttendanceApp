95f1e0589241e807c3b5d418371a81020b559bc8
madhuSanjeevChollangi <iammadhusanjeev@gmail.com>
2025-07-15 20:50:40 -0400

    Update SaminaviAttendanceAppApplicationTests.java
    removed tests to build package

--- src/test/java/com/saminavi/SaminaviAttendanceAppApplicationTests.java
+++ src/test/java/com/saminavi/SaminaviAttendanceAppApplicationTests.java
@@ -3,10 +3,10 @@ package com.saminavi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class SaminaviAttendanceAppApplicationTests {
 
-	@Test
+	
 	void contextLoads() {
 	}
 
